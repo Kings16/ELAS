52a51c3326f867516fa7536e8986297a738af4af
Zahraalagh <104637345+Zahraalagh@users.noreply.github.com>
2022-05-29 13:20:25 +0200

    Update course.py

--- backend/orm_interface/entities/course.py
+++ backend/orm_interface/entities/course.py
@@ -1,50 +1,23 @@
-from sqlalchemy import Column, Float, Integer, String
-from sqlalchemy.sql.sqltypes import Boolean
+from sqlalchemy import Boolean, Column, Integer, String, Float, ForeignKey
 from orm_interface.base import Base
+from sqlalchemy.orm import relationship
+ 
 
-class Course(Base):
-    __tablename__ = "course"
-    id = Column (Integer, primary_key=True)
-    selected = Column(Boolean)
-    title = Column(String)
-    link: Column(String)
-    catalog: Column(String)
-    type: Column(String)
-    sws: Column(Integer)
-    erwartete_teilnehmer:  Column(Integer)
-    max_teilnehmer:  Column(Integer)
-    credits:  Column(Integer)
-    language: Column(String)
-    description: Column(String)
-    times_manual: Column(String)
-    location: Column(String)
-    exam: Column(String)
-    ausgeschlossen_ingenieurwissenschaften_bachelor: Column(String)
-    fairness: Column(Float)
-    support: Column(Float)
-    material: Column(Float)
-    fun: Column(Float)
-    comprehensibility: Column(Float)
-    interesting: Column(Float)
-    grade_effort: Column(Float)
+class E3_Rating(Base):
+    __tablename__ = "e3_rating"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    fairness = Column(Float)
+    support = Column(Float)
+    material = Column(Float)
+    fun = Column(Float)
+    comprehensibility = Column(Float)
+    interesting = Column(Float) 
+    grade_effort = Column(Float)
+    e3_course_id = Column(Integer, ForeignKey('e3_courses.id'))
 
-    def __init__(self, id :int ,selected: bool, title: str, link: str, catalog: str, type: str, sws: int, erwartete_teilnehmer: int, max_teilnehmer: int, credits: int, language: str, description: str, times_manual: str, location: str, exam: str, ausgeschlossen_ingenieurwissenschaften_bachelor: str, fairness: float, support: float, material: float, fun: float, comprehensibility: float, interesting: float, grade_effort: float) -> None:
-        self.id = id
-        self.selected = selected
-        self.title = title
-        self.link = link
-        self.catalog = catalog
-        self.type = type
-        self.sws = sws  
-        self.erwartete_teilnehmer = erwartete_teilnehmer
-        self.max_teilnehmer = max_teilnehmer
-        self.credits = credits
-        self.language = language
-        self.description = description
-        self.times_manual = times_manual
-        self.location = location
-        self.exam = exam
-        self.ausgeschlossen_ingenieurwissenschaften_bachelor = ausgeschlossen_ingenieurwissenschaften_bachelor
+    def __init__(self, fairness, support, 
+        material, fun, comprehensibility, 
+        interesting, grade_effort,e3_course_id ):
         self.fairness = fairness
         self.support = support
         self.material = material
@@ -52,3 +25,46 @@ class Course(Base):
         self.comprehensibility = comprehensibility
         self.interesting = interesting
         self.grade_effort = grade_effort
+        self.e3_course_id=e3_course_id
+      
+
+class E3_Courses(Base):
+    __tablename__ = "e3_courses"
+ 
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    selected = Column(Boolean)
+    name = Column(String)
+    url = Column(String)
+    catalog = Column(String)
+    type = Column(String)
+    sws= Column(String) 
+    num_expected_participants = Column(String) 
+    max_participants = Column(String) 
+    credit = Column(String) 
+    language = Column(String)
+    description = Column(String)
+    location = Column(String)
+    exam_type = Column(String)
+    time_manual = Column(String)
+    ausgeschlossen_ingenieurwissenschaften_bachelor = Column(String)
+    e3_rating = relationship(E3_Rating, backref="e3_courses")
+    def __init__(self, selected, name, url, catalog, subject_type, 
+                sws, num_expected_participants, 
+                max_participants, credit,language,
+                description, location, exam_type, time_manual,
+                ausgeschlossen_ingenieurwissenschaften_bachelor):
+         self.selected = selected
+         self.name = name
+         self.url = url
+         self.catalog = catalog
+         self.subject_type = subject_type
+         self.sws = sws
+         self.num_expected_participants = num_expected_participants
+         self.max_participants = max_participants
+         self.credit = credit
+         self.language = language
+         self.description = description
+         self.location = location
+         self.exam_type = exam_type
+         self.time_manual = time_manual
+         self.ausgeschlossen_ingenieurwissenschaften_bachelor = ausgeschlossen_ingenieurwissenschaften_bachelor
